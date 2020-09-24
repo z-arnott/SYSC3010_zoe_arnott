4d2b02863c17a788e0b8beec8f5ee62f2af380f7
Zoe Arnott <zoe.arnott@carleton.ca>
2020-09-24 09:04:39 -0400

    Doc: Has RPi pin names

--- Lab-2/lab2-hardware-step3.py
+++ Lab-2/lab2-hardware-step3.py
@@ -1,21 +1,23 @@
 # Powers and reads digital output from Soil Moisture YL69 with HL69 breakout board
 import RPi.GPIO as GPIO
 
+
 GPIO.setmode(GPIO.BCM)
 #name pins
 power_out = 2
 digital_in = 4
 button = 15
-
 #pin setup
-GPIO.setup(2, GPIO.OUT)
-GPIO.setup(4, GPIO.IN)
-GPIO.setup(15, GPIO.IN)
+GPIO.setup(power_out, GPIO.OUT)
+GPIO.setup(digital_in, GPIO.IN)
+GPIO.setup(button, GPIO.IN)
+
 
 
 def check_moisture():
     GPIO.output(power_out, HIGH)
     return GPIO.input(digital_in, HIGH)
+
 
 while true:
     # button is active low
