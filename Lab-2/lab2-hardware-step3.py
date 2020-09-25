e6434424338d783863de5c8b9259c602033c4389
Zoe Arnott <zoe.arnott@carleton.ca>
2020-09-24 22:38:09 -0400

    fix: Delay after button press
    Delay after button press prevents multiple function calls on single button press.

--- Lab-2/lab2-hardware-step3.py
+++ Lab-2/lab2-hardware-step3.py
@@ -1,6 +1,8 @@
 # Powers and reads digital output from Soil Moisture YL69 with HL69 breakout board
 import RPi.GPIO as GPIO
-
+import time
+import httplib
+import urllib
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -18,7 +20,6 @@ GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def check_moisture():
     GPIO.output(power_out, GPIO.HIGH)
     is_moist = GPIO.input(digital_in)
-    GPIO.output(power_out, GPIO.LOW)
     return is_moist
 
 while True:
@@ -27,3 +28,5 @@ while True:
           print("plant is MOIST")
        else:
            print("plant is thirsty")
+    #delay 100 ms to prevent duplicate button presses
+    time.sleep(0.1)
