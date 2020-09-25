1992ef271fe8195bf640852a59fed606a3b984a5
Zoe Arnott <zoe.arnott@carleton.ca>
2020-09-24 23:39:21 -0400

    Feat: Post request to ThingSpeak
    Program sends post request with sensor data to ThingSpeak on button click

--- Lab-2/lab2-hardware-step3.py
+++ Lab-2/lab2-hardware-step3.py
@@ -4,29 +4,41 @@ import time
 import httplib
 import urllib
 
+key = "D222ZGCJNCGK29TD"
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-#name pins
+# name pins
 power_out = 2
 digital_in = 4
 button = 15
-#pin setup
+# pin setup
 GPIO.setup(power_out, GPIO.OUT)
 GPIO.setup(digital_in, GPIO.IN)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-
 def check_moisture():
     GPIO.output(power_out, GPIO.HIGH)
+    time.sleep(0.1)
     is_moist = GPIO.input(digital_in)
+    GPIO.output(power_out, GPIO.LOW)
     return is_moist
 
+
 while True:
-   if GPIO.input(button):
-       if check_moisture():
-          print("plant is MOIST")
-       else:
-           print("plant is thirsty")
-    #delay 100 ms to prevent duplicate button presses
-    time.sleep(0.1)
+    if GPIO.input(button):
+        moisture = check_moisture()
+        print(moisture)
+        param = urllib.urlencode({'field1': moisture, 'key':key})
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", param)
+            response = conn.getresponse()
+            print(response.status, response.reason)
+            data = response.read()
+            conn.close()
+        except:
+            print("connection failed")
+        # delay 100 ms to prevent duplicate button presses
+        time.sleep(0.1)
