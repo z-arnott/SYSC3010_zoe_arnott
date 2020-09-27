c6992f043061d2c761e05ef94deb5c18758f310a
Zoe Arnott <zoe.arnott@carleton.ca>
2020-09-26 22:51:47 -0400

    Feat: Request data from ThingSpeak

--- Lab-2/lab2-thingspeak-step3.py
+++ Lab-2/lab2-thingspeak-step3.py
@@ -1,2 +1,10 @@
 # Read simulated ECG data from ThingSpeak channel
+import requests
 
+
+try:
+    r = requests.get("https://api.thingspeak.com/channels/1156912/fields/2.json?api_key=W9SMONCUM7N2440D&results=2")
+    print(r.status_code)
+    print(r.json())
+except:
+    print("failed")
