7651e6c83469c8ec60fea70be2fe5820c366e626
Zoe Arnott <zoe.arnott@carleton.ca>
2020-10-01 18:05:40 -0400

    Fix: Print values instead of Object address

--- Lab-3/lab4-database-demo.py
+++ Lab-3/lab4-database-demo.py
@@ -3,29 +3,33 @@
 import sqlite3
 
 #some initial data
-id = 4;
-temperature = 0.0;
-date = '2014-01-05';
+id = 4
+temperature = 0.0
+date = '2014-01-05'
 #connect to database file
-dbconnect = sqlite3.connect("my.db");
+dbconnect = sqlite3.connect("my.db")
+
+
 #If we want to access columns by name we need to set
 #row_factory to sqlite3.Row class
-dbconnect.row_factory = sqlite3.Row;
-cursor = dbconnect.cursor();
+# dbconnect.row_factory = sqlite3.Row
+cursor = dbconnect.cursor()
+cursor.execute("CREATE TABLE temperature(id INTEGER PRIMARY KEY, temp TEXT, date DATETIME)")
+
 for i in range(10):
     #execute insert statement
-    id += 1;
-    temperature += 1.1;
-    cursor.execute('''insert into temperature values (?, ?, ?)''',
-    (id, temperature, date));
-dbconnect.commit();
-#execute simple select statement
-cursor.execute('SELECT * FROM temperature');
+    id += 1
+    temperature += 1.1
+    cursor.execute("insert into temperature values (?, ?, ?)",(id, temperature, date))
+
+dbconnect.commit()
 #print data
-for row in cursor:
-    print(row['id'],row['temp'],row['date'] );
+for row in cursor.execute('SELECT * FROM temperature'):
+    print(row)
+
+
 #close the connection
-dbconnect.close();
+dbconnect.close()
 
 # New Code for Lab3
 def create_sensor(conn, sensor):
@@ -47,12 +51,14 @@ cursor = conn.cursor()
 cursor.execute("CREATE TABLE sensors(sensorID INTEGER PRIMARY KEY, type TEXT, zone TEXT)")
 
 # Add rows to sensors table
-create_sensor(conn, (door, kitchen))
-create_sensor(conn, (temperature, kitchen))
-create_sensor(conn, (door, garage))
-create_sensor(conn, (motion, garage))
-create_sensor(conn, (temperature, garage))
-print(cursor.fetchall())
+create_sensor(conn, ("door", "kitchen"))
+create_sensor(conn, ("temperature", "kitchen"))
+create_sensor(conn, ("door", "garage"))
+create_sensor(conn, ("motion", "garage"))
+create_sensor(conn, ("temperature", "garage"))
+
+for row in cursor.execute('SELECT * FROM sensors'):
+    print(row)
 
 conn.commit()
 conn.close()
