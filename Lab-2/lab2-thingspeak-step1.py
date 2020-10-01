52447e66fdf815664603fb7b0325c66d256069a4
Zoe Arnott <zoe.arnott@carleton.ca>
2020-10-01 13:05:07 -0400

    Refactor: Change program to Python3

--- Lab-2/lab2-thingspeak-step1.py
+++ Lab-2/lab2-thingspeak-step1.py
@@ -1,22 +1,18 @@
 # POST simulated ECG data to ThingSpeak channel
-import httplib
-import urllib
+import requests
 import time
 
 
 key = "D222ZGCJNCGK29TD"
 
 #Simulated ECG data points
-x = [40, 38, 42, 43, 44, 45, 44, 43, 42,41, 40, 40, 40, 40, 40, 39, 42, 43, 60, 80, 100, 120, 80, 50, 5, 10, 20, 30, 40, 50, 35, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 51, 50, 49, 48, 45, 42, 40, 40]
+data_x = [40, 38, 42, 43, 44, 45, 44, 43, 42,41, 40, 40, 40, 40, 40, 39, 42, 43, 60, 80, 100, 120, 80, 50, 5, 10, 20, 30, 40, 50, 35, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 51, 50, 49, 48, 45, 42, 40, 40]
 
-for data in x:
-    param = urllib.urlencode({'field2': data, 'key':key})
-    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+for x in data_x:
+
     try:
-        conn.request("POST", "/update", param)
-        response = conn.getresponse()
-        print (response.status, response.reason)
-        conn.close()
+        r = requests.post("https://api.thingspeak.com/update?api_key=D222ZGCJNCGK29TD&field1={x}")
+        print (r.status_code)
     except:
         print("failed")
     # Delay next POST request for 15 seconds, min update interval on ThingSpeak
